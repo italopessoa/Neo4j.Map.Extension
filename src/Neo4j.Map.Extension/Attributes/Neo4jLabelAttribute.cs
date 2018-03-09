672c2822641da0fbe14e2a89f8b02b489ded876e
Italo Pessoa <italo.pessoa@hotmail.com>
2018-03-08 23:29:38 -0300

    add attribute classes

---
+++ src/Neo4j.Map.Extension/Attributes/Neo4jLabelAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Neo4j.Map.Extension.Attributes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class Neo4jLabelAttribute : Attribute
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        public Neo4jLabelAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ src/Neo4j.Map.Extension/Attributes/Neo4jPropertyAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Neo4j.Map.Extension.Attributes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class Neo4jPropertyAttribute : Attribute
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
